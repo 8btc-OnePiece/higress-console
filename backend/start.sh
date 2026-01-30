6a50082f544be3d849f597e520465165dd587d38
ailong <zicen@dian.so>
2026-01-30 16:52:47 +0800

    启动新增环境标

--- backend/start.sh
+++ backend/start.sh
@@ -2,7 +2,7 @@
 echo $1
 if [ "$1" = "--local" ]
 then
-    java $JVM_ARGS -jar ./console/target/higress-console.jar
+    java $JVM_ARGS -Dspring.profiles.active=$SPRING_PROFILES_ACTIVE -jar ./console/target/higress-console.jar
 else
-    java $JVM_ARGS -jar /app/higress-console.jar
+    java $JVM_ARGS -Dspring.profiles.active=$SPRING_PROFILES_ACTIVE -jar /app/higress-console.jar
 fi
